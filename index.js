95d8ddca8656632bfc08ba36d781807e954bbf73
Sachin Gavali <sachingavali321@gmail.com>
2024-06-15 09:20:16 +0530

    file system in node js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require("http");
+/* const http = require("http");
 
 
 const server = http.createServer((req , res) => {
@@ -12,4 +12,40 @@ const server = http.createServer((req , res) => {
 
 server.listen(4000 , () => {
     console.log("Server listen on 4000 port ");
-});
+}); */
+
+// file system in node js 
+
+ const fs = require("fs");
+/*
+
+// write file method 
+// async file :-> excute when it will load  another functions are excute 
+fs.writeFile("./data.txt" , " hello Check it " , "utf-8" , () => {
+console.log("File is created");
+} );
+
+
+// sync file 
+fs.writeFileSync("./dataSync.txt" , " This is Synchronous File data " , "utf-8") */
+
+
+// apppend file method
+// async file 
+/* fs.appendFile("./data.txt" , " New Data add into file " , "utf-8" , () => {
+    console.log(" Data add into file");
+}) */
+
+
+//rename file 
+
+/* fs.rename("./data.txt" ,"newFile.txt" , () =>{
+    console.log("File Name is Updated ");
+}) */
+
+
+// read  file     
+/* 
+const data = fs.readFileSync("./newFile.txt" , "utf-8");
+
+console.log(data); */
